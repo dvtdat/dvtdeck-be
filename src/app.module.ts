840dfeffe8b9d0f283ebbdcda6276f4e9d04510f
dvtdat <tiendat0417@gmail.com>
2025-06-02 08:50:56 +0700

    chore: inject auth module into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import config from './mikro-orm.config';
 import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [MikroOrmModule.forRoot(config), UserModule],
+  imports: [MikroOrmModule.forRoot(config), UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UserModule } from '../user/user.module';
 
 @Module({
   imports: [UserModule],
@@ -8,4 +9,4 @@ import { AuthService } from './auth.service';
   controllers: [AuthController],
   exports: [AuthService],
 })
-export class UserModule {}
+export class AuthModule {}
