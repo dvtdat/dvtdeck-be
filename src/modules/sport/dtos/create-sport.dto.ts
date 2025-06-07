7e0accfb6d64f042219a45091b0df277e3092e27
dvtdat <tiendat0417@gmail.com>
2025-06-07 18:37:49 +0700

    chore: add swagger to create dtos

--- src/modules/sport/dtos/create-sport.dto.ts
+++ src/modules/sport/dtos/create-sport.dto.ts
@@ -1,4 +1,9 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
 export class CreateSportDto {
+  @ApiProperty({ description: 'The name of the sport' })
   name: string;
+
+  @ApiPropertyOptional({ description: 'A brief description of the sport' })
   description?: string;
 }
--- src/modules/user/dtos/create-user.dto.ts
+++ src/modules/user/dtos/create-user.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({ description: 'The username of the user' })
   username: string;
+
+  @ApiProperty({ description: 'The email of the user' })
   email: string;
+
+  @ApiProperty({ description: 'The password of the user' })
   password: string;
 }
